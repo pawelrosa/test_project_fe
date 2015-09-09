f274339ff770d4f3d0a700892021d1c880c3f0aa
Paweł Rosa <pawel.tomasz.rosa@gmail.com>
2015-09-09 08:33:38 +0200

    Fixing assets on production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ role :app, %w{paulus@46.101.150.244}
 role :web, %w{paulus@46.101.150.244}
 role :db,  %w{paulus@46.101.150.244}
 set :deploy_to, '/home/paulus/www/test_project_fe/production'
-set :branch, 'master'
+set :branch, 'deployment'
 
 server '46.101.150.244', user: 'paulus', roles: %w{web app}
